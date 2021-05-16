fe91e80d398b05fe012d019ca034eefbcb055f27
Pier Philippon <pier.philippon@gmail.com>
2021-05-17 00:13:31 +0200

    creation de la page d'affichage d'une annonce spécifique

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +12,27 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo): Response
     {
-    	$repo = $this->getDoctrine()->getRepository(Ad::class);
-
     	$ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
     }
+
+	/**
+	 * Permet d'afficher une seule annonce
+	 *
+	 * @Route("/ads/{slug}", name="ads_show")
+	 * @return Response
+	 * @author Pier
+	 */
+    public function show($slug, AdRepository $repo): Response {
+    	$ad = $repo->findOneBySlug($slug);
+
+		return $this->render("ad/show.html.twig", [
+			"ad" => $ad
+		]);
+	}
 }
