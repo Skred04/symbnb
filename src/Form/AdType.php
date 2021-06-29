d47b257d7e7695c61074d289661c0bdaa6e6965f
Pier Philippon <pier.philippon@gmail.com>
2021-06-29 19:13:37 +0200

    refactor: ordering input form AdType

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -34,11 +34,11 @@ class AdType extends AbstractType
         $builder
             ->add('title', TextType::class, $this->getConfiguration('Titre', 'Tapez le titre de votre annonce'))
             ->add('slug', TextType::class, $this->getConfiguration('Adresse Web', 'Tapez l\'adresse web (automatique)'))
-            ->add('price', MoneyType::class, $this->getConfiguration('Prix par nuit', 'Indiquez le prix que vous voulez pour une nuit'))
+            ->add('coverImage', UrlType::class, $this->getConfiguration('URL de l\'image principale', 'URL de votre image de couverture'))
             ->add('introduction', TextType::class, $this->getConfiguration('Introduction', 'Donnez une description globale de l\'annonce'))
             ->add('content', TextareaType::class, $this->getConfiguration('Description détaillée', 'Tapez une description qui donne envie de venir chez vous'))
-            ->add('coverImage', UrlType::class, $this->getConfiguration('URL de l\'image principale', 'URL de votre image de couverture'))
             ->add('rooms', IntegerType::class, $this->getConfiguration('Nombre de chambres', 'Le nombre de chambres disponibles'))
+            ->add('price', MoneyType::class, $this->getConfiguration('Prix par nuit', 'Indiquez le prix que vous voulez pour une nuit'))
         ;
     }
 
