90194a184624f55f375981ba538ba27af5e9f9d5
Pier Philippon <pier.philippon@gmail.com>
2021-07-02 17:51:57 +0200

    feat: add images collection and js to append and remove them

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -39,6 +40,13 @@ class AdType extends AbstractType
             ->add('content', TextareaType::class, $this->getConfiguration('Description détaillée', 'Tapez une description qui donne envie de venir chez vous'))
             ->add('rooms', IntegerType::class, $this->getConfiguration('Nombre de chambres', 'Le nombre de chambres disponibles'))
             ->add('price', MoneyType::class, $this->getConfiguration('Prix par nuit', 'Indiquez le prix que vous voulez pour une nuit'))
+            ->add('images',
+                CollectionType::class,
+                [
+                    'entry_type' => ImageType::class,
+                    'allow_add' => true,
+                ]
+            )
         ;
     }
 
