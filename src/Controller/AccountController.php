25099434650688748e9bc3be94c101995e34685d
Pier Philippon <pier.philippon@gmail.com>
2021-07-07 21:59:30 +0200

    feat: add registration form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,4 +33,20 @@ class AccountController extends AbstractController
      * @return Void
      */
     public function logout(): Void{}
+
+    /**
+     * Permet d'afficher le formulaire d'inscription
+     * @Route("/register", name="account_register")
+     * @return Response
+     */
+    public function register(): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render("account/registration.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
 }
