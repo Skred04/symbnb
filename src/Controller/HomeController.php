ecc11775fce260b50a109fb567e45f1bfe98805a
Pier Philippon <pier.philippon@gmail.com>
2021-08-23 18:50:27 +0200

    add dashboard and dql course

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,39 +1,20 @@
 <?php
 	namespace App\Controller;
 
-	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use App\Repository\AdRepository;
+    use App\Repository\UserRepository;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
 
 	Class HomeController extends AbstractController {
-
-		/**
-		 * @Route ("/bonjour/{prenom}/age/{age}", name="hello")
-		 * @Route ("/bonjour", name="hello_base")
-		 * @Route ("/bonjour/{prenom}", name="hello_prenom")
-		 * Montre la page qui dit bonjour
-		 */
-		public function hello($prenom = "test", $age = 0){
-			return $this->render(
-				"hello.html.twig",
-				[
-					"prenom" => $prenom,
-					"age" => $age
-				]
-			);
-		}
 		/**
 		 * @Route("/", name="homepage")
 		 */
-		public function home() {
-			$prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" => 3];
-			return $this->render(
-				"home.html.twig",
-				[
-					"title" => "Au revoir tout le monde",
-					"age" => 3,
-					"tableau" => $prenoms,
-				]
-			);
+		public function home(AdRepository $adRepo, UserRepository $userRepo) {
+			return $this->render("home.html.twig", [
+			    "ads" => $adRepo->findBestAds(3),
+                "users" => $userRepo->findBestUsers()
+            ]);
 		}
 	}
